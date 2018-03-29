da1be6dab3ac277ab793c891d0ad082760727602
leoyanzhizhang <fantasyleonzhang@hotmail.com>
2018-03-28 22:01:47 -0400

    updatea

--- assignment/a4-code-v2-updated/cycle_gan.py
+++ assignment/a4-code-v2-updated/cycle_gan.py
@@ -277,7 +277,7 @@ def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader
         if opts.use_cycle_consistency_loss:
             reconstructed_Y = G_XtoY(fake_X)
             # 3. Compute the cycle consistency loss (the reconstruction loss)
-            cycle_consistency_loss = ((D_Y(reconstructed_Y)-1)**2).mean()
+            cycle_consistency_loss = (images_Y - reconstructed_Y)**2).sum()/opts.batch_size
             g_loss += cycle_consistency_loss
 
         g_loss.backward()
@@ -300,7 +300,7 @@ def training_loop(dataloader_X, dataloader_Y, test_dataloader_X, test_dataloader
         if opts.use_cycle_consistency_loss:
             reconstructed_X = G_YtoX(fake_Y)
             # 3. Compute the cycle consistency loss (the reconstruction loss)
-            cycle_consistency_loss = ((D_X(reconstructed_X)-1)**2).mean()
+            cycle_consistency_loss = (images_X - reconstructed_X)**2).sum()/opts.batch_size
             g_loss += cycle_consistency_loss
 
         g_loss.backward()
